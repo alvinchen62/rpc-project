e9c3fe467ae280b43ff5c35a19d0882122b88c62
Alvin Chen <pc56@uw.edu>
2025-01-09 23:01:51 -0600

    rpc-easy-dynamic_proxy

--- example-consumer/src/main/java/com/yupi/example/consumer/EasyConsumerExample.java
+++ example-consumer/src/main/java/com/yupi/example/consumer/EasyConsumerExample.java
@@ -2,7 +2,7 @@ package com.yupi.example.consumer;
 
 import com.yupi.example.common.model.User;
 import com.yupi.example.common.service.UserService;
-//import com.yupi.yurpc.proxy.ServiceProxyFactory;
+import com.yupi.yurpc.proxy.ServiceProxyFactory;
 
 /**
  * 简易服务消费者示例
@@ -10,12 +10,16 @@ import com.yupi.example.common.service.UserService;
 public class EasyConsumerExample {
 
     public static void main(String[] args) {
-        // todo 需要获取 UserService 的实现类对象
+
+        // 动态代理
+        UserService userServiceDynamic = ServiceProxyFactory.getProxy(UserService.class);
+
+        // 静态代理
         UserService userService = new UserServiceProxy();
         User user = new User();
-        user.setName("yupi_testing");
+        user.setName("yupi_testing_666");
         // 调用
-        User newUser = userService.getUser(user);
+        User newUser = userServiceDynamic.getUser(user);
         if (newUser != null) {
             System.out.println(newUser.getName());
         } else {
