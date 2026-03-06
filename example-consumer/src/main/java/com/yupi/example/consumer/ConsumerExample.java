0fe24c9e889e325362243bff117e90e1b1b34587
Alvin Chen <alvcpy621@gmail.com>
2026-03-05 21:32:21 -0600

    rpc-core-mock_interface

--- example-consumer/src/main/java/com/yupi/example/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/yupi/example/consumer/ConsumerExample.java
@@ -1,7 +1,8 @@
 package com.yupi.example.consumer;
 
-import com.yupi.yurpc.config.RpcConfig;
-import com.yupi.yurpc.utils.ConfigUtils;
+import com.yupi.example.common.model.User;
+import com.yupi.example.common.service.UserService;
+import com.yupi.yurpc.proxy.ServiceProxyFactory;
 
 /**
  * 简易服务消费者示例
@@ -13,7 +14,18 @@ import com.yupi.yurpc.utils.ConfigUtils;
 public class ConsumerExample {
 
     public static void main(String[] args) {
-        RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
-        System.out.println(rpc);
+        // 获取代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("yupi");
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null) {
+            System.out.println(newUser.getName());
+        } else {
+            System.out.println("user == null");
+        }
+        long number = userService.getNumber();
+        System.out.println(number);
     }
 }
